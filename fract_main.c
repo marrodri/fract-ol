9dbd9b657570987f9a832fd955e64fbc99af42fa
marrodri <laza.mar64@gmail.com>
2019-07-22 16:29:28 -0700

    added pixel

--- fract_main.c
+++ fract_main.c
@@ -2,6 +2,14 @@
 
 #include "fract.h"
 
+int		mlx_pixel_image(int x, int y, char *addr, int bpp)
+{
+	if (x < 0 || y < 0 || x >= WIN_SZ || y >= WIN_SZ)
+		return (0);
+	*((int *)(addr + (x + y * WIN_SZ) * bpp)) = 0x80ff00;
+	return (1);
+}
+
 int main(int argc, char **argv)
 {
 	void *p_mlx;
@@ -18,6 +26,8 @@ int main(int argc, char **argv)
 	st_img->addr = mlx_get_data_addr(p_img, &st_img->bpp, 
 		&st_img->sl, &st_img->endn);
 	st_img->bpp /= 8;
+	mlx_pixel_image(500,500, st_img->addr, st_img->bpp);
+	mlx_put_image_to_window(p_mlx, p_win, p_img, 0, 0);
 
 	mlx_loop(p_mlx);
 	return 0;
